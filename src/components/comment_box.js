e41c5cad76da882f2e8de6a291fdc3140a6dcd85
Thibault Souef <souef.thibault@gmail.com>
2017-09-21 00:16:19 +0200

    wip learn testing

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div>
+        <textarea />
+        <button>Submit comment</button>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -8,7 +8,8 @@ describe('CommentBox', () => {
     expect(component).find('textarea').to.exist;
   });
 
-  it('has a button'), () => {
-
-  };
+  it('has a button', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).find('button').to.exist;
+  }) ;
 })
